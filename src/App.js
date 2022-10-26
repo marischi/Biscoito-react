348a936cee93193f2d93f6031a3ad48f8d8d072e
marischi <marileneifsp@gmail.com>
2022-10-25 21:48:15 -0300

    Base do projeto finalizado

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+
+    this.frases = [
+      'Siga os bons e aprenda com eles.',
+      'O bom-senso vale mais do que muito conhecimento.',
+      'O riso é a menor distância entre duas pessoas.',
+      'Deixe de lado as preocupações e seja feliz.',
+      'Realize o óbvio, pense no improvável e conquiste o impossível.',
+      'Acredite em milagres, mas não dependa deles.',
+      'A maior barreira para o sucesso é o medo do fracasso.',
+    ];
+  }
+  render() {
+    return (
+      <div>
+        <img src={require('./assets/biscoito.png')} alt="biscoito"></img>
+        <Botao />
+        <h3>Frase aleatória...</h3>
+      </div>
+    );
+  }
+}
+
+class Botao extends Component {
+  render() {
+    return (
+      <div>
+        <button>Abrir biscoito</button>
+      </div>
+    );
+  }
 }
 
 export default App;
